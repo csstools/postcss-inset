0e9483d2fb6fe18832d904ea09e1232ffc905f1e
Lubomír Blažek <evromalarkey@gmail.com>
2021-06-14 20:51:46 +0200

    Support PostCSS 8 (#2)

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-// tooling
-const postcss = require('postcss');
+const creator = () => {
+	// side properties
+	const properties = ['top', 'right', 'bottom', 'left'];
 
-// side properties
-const properties = ['top', 'right', 'bottom', 'left'];
+	return {
+		postcssPlugin: 'postcss-inset',
+		Declaration: {
+			'inset': (decl, { list }) => {
+				// space-separated side values (top, right, bottom, left)
+				const values = list.space(decl.value);
 
-// inset plugin
-module.exports = postcss.plugin('postcss-inset', () => (root) => {
-	root.walkDecls('inset', (decl) => {
-		// space-separated side values (top, right, bottom, left)
-		const values = postcss.list.space(decl.value);
+				// conditionally add a right value
+				if (values.length === 1) {
+					values.push(values[0]);
+				}
 
-		// conditionally add a right value
-		if (values.length === 1) {
-			values.push(values[0]);
+				// conditionally add a bottom value
+				if (values.length === 2) {
+					values.push(values[0]);
+				}
+
+				// conditionally add a left value
+				if (values.length === 3) {
+					values.push(values[1]);
+				}
+
+				// only transform up to 4 side values
+				if (values.length === 4) {
+					// for each side property
+					properties.forEach((property, index) => {
+						// create a new declaration for the side
+						decl.cloneBefore({
+							prop: property,
+							value: values[index]
+						});
+					});
+				}
+
+				decl.remove();
+			},
 		}
+	}
+}
 
-		// conditionally add a bottom value
-		if (values.length === 2) {
-			values.push(values[0]);
-		}
+creator.postcss = true
 
-		// conditionally add a left value
-		if (values.length === 3) {
-			values.push(values[1]);
-		}
-
-		// only transform up to 4 side values
-		if (values.length === 4) {
-			// for each side property
-			properties.forEach((property, index) => {
-				// create a new declaration for the side
-				decl.cloneBefore({
-					prop: property,
-					value: values[index]
-				});
-			});
-		}
-
-		decl.remove();
-	});
-});
+export default creator
